f05c8776aa6c431199f63f2d7e0308240753c165
JusTMeth25 <lorenzo.melis@yahoo.it>
2026-06-29 17:42:45 +0200

    commit

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,9 +1,19 @@
-const Images = function (props) {
-  return (
-    <div className="images">
-      <img src={props.src} alt="foto react" />
-    </div>
-  );
-};
+// const Images = function (props) {
+//   return (
+//     <div className="images">
+//       <img src={props.src} alt="foto react" />
+//     </div>
+//   );
+// };
+
+// export default Images;
+
+import { Component } from "react";
+
+class Images extends Component {
+  render() {
+    return <img src={this.props.src} alt={this.props.altDesc} className="images" />;
+  }
+}
 
 export default Images;
